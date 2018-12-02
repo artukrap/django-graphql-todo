ab3ec67a27bd22450982c688a902228887086c3f
Arturs Krapans <akrapans@scoutrfp.com>
2018-12-02 01:17:08 +0100

    Add docstrings

--- issues/models.py
+++ issues/models.py
@@ -1,15 +1,19 @@
 from django.db import models
 
 class Issue(models.Model):
+    """Issue model object"""
+
     title = models.CharField(max_length=200, blank=False)
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
 
 class Comment(models.Model):
+    """Comment model object"""
+
     issue = models.ForeignKey(Issue, on_delete=models.CASCADE, null=True)
     message = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
